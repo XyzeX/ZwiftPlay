b85275233fec1a6f7e0edacffae5b44442b73436
XyzeX <60699942+XyzeX@users.noreply.github.com>
2025-10-05 23:32:55 +0200

    Initial commit

---
+++ App/Build-App.lua
@@ -0,0 +1,41 @@
+project "App"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    targetdir "bin/%{cfg.buildcfg}"
+    staticruntime "off"
+
+    files
+    {
+        "Source/**.h",
+        "Source/**.cpp"
+    }
+
+    includedirs
+    {
+        "Source"
+    }
+
+    targetdir ("../bin/" .. OutputDir .. "/%{prj.name}")
+    objdir ("../bin/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+    filter "system:windows"
+        systemversion "latest"
+        defines { "WINDOWS" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        runtime "Release"
+        optimize "On"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines { "DIST" }
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
